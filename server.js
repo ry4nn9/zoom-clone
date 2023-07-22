c45b855090b60c558837d33d6c266259d64d8331
ry4nn9 <ryanng2688@gmail.com>
2023-07-21 22:09:22 -0700

    Set up server for socket

---
+++ server.js
@@ -0,0 +1,35 @@
+
+const express = require('express')
+const app = express()
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidV4()}`)
+})
+
+// create route for room
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId)
+        socket.to(roomId).emit('user-connected', userId)
+
+        socket.on('disconnect', () => {
+            socket.to(roomId).emit('user-disconnected', userId)
+        })
+    })
+})
+
+// starts server on port 3000
+server.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
